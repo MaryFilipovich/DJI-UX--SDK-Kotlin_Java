4f4a7c25e69c4f62f5728736a77e32e3b898d070
Mary <maria0filipovich@gmail.com>
2020-01-29 12:08:42 +0300

    Initial commit

---
+++ app/src/main/java/com/work/djiuxmobilesdkkotlin_java/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.work.djiuxmobilesdkkotlin_java
+
+import android.Manifest
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // When the compile and target version is higher than 22, please request the
+        // following permissions at runtime to ensure the
+        // SDK work well.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.VIBRATE,
+                Manifest.permission.INTERNET, Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
+                Manifest.permission.READ_PHONE_STATE), 1)
+        }
+
+        setContentView(R.layout.activity_main)
+    }
+}
